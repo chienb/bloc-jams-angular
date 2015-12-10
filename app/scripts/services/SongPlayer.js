c889b8b52fa9f191146fceccf9022d5d4dc3abd5
Brett Chien <brebre_no7@yahoo.com>
2015-12-09 23:22:35 -0800

    assignment7-2

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -1,27 +1,50 @@
  (function() {
      function SongPlayer() {
+          
           var SongPlayer = {};
           var currentSong = null;
+          /**
+          * @desc Buzz object audio file
+          * @type {Object}
+          */
           var currentBuzzObject = null;
 
+          /**
+          * @function setSong
+          * @desc Stops currently playing song and loads new audio file as currentBuzzObject
+          * @param {Object} song
+          */
+          var setSong = function(song) {
+              if (currentBuzzObject) {
+                  currentBuzzObject.stop();
+                  currentSong.playing = null;
+              }
+           
+              currentBuzzObject = new buzz.sound(song.audioUrl, {
+                  formats: ['mp3'],
+                  preload: true
+              });
+           
+              currentSong = song;
+          };
+
+          /**
+          * @function playSong
+          * @desc Plays the currentBuzzObject audio file
+          * @param {Object} song
+          */
+          var playSong = function (song) {
+                currentBuzzObject.play();
+                song.playing = true; 
+          };
 
           SongPlayer.play = function(song) {
             if (currentSong !== song) {
-                if (currentBuzzObject) {
-                    currentBuzzObject.stop();
-                    currentSong.playing = null;
-                }
-                currentBuzzObject = new buzz.sound(song.audioUrl, {
-                  formats: ['mp3'],
-                  preload: true
-                });
-                currentSong = song;
-                currentBuzzObject.play();
-                song.playing = true;    
+                setSong(song);
+                playSong(song);   
             } else if (currentSong === song) {
                 if (currentBuzzObject.isPaused()) {
-                    currentBuzzObject.play();
-                    song.playing = true;
+                    playSong(song);
                 }
               } 
           };
