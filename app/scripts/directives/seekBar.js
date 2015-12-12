444a55b7d3319341272d7d4b85d84ee47cdf6a5f
Brett Chien <brebre_no7@yahoo.com>
2015-12-11 23:15:34 -0800

    checkpoint10

--- app/scripts/directives/seekBar.js
+++ app/scripts/directives/seekBar.js
@@ -13,12 +13,22 @@
     	    templateUrl: '/templates/directives/seek_bar.html',
     	    replace: true,
     	    restrict: 'E',
-    	    scope: { },
+            scope: {
+                onChange: '&'
+            },
     	    link: function(scope, element, attributes) {
     	        scope.value = 0;
     	        scope.max = 100;
     	        
     	        var seekBar = $(element);
+
+                attributes.$observe('value', function(newValue) {
+                    scope.value = newValue;
+                });
+                
+                attributes.$observe('max', function(newValue) {
+                    scope.max = newValue;
+                });
 
 
     	        var percentString = function () {
@@ -36,11 +46,11 @@
                     return {left: percentString()};
                 };
 
-
-
     	        scope.onClickSeekBar = function(event) {
     	            var percent = calculatePercent(seekBar, event);
     	            scope.value = percent * scope.max;
+                    notifyOnChange(scope.value);
+
     	        };
 
     	        scope.trackThumb = function() {
@@ -48,6 +58,8 @@
     	                var percent = calculatePercent(seekBar, event);
     	                scope.$apply(function() {
     	                    scope.value = percent * scope.max;
+                            notifyOnChange(scope.value);
+
     	                });
     	            });
     	        
@@ -55,7 +67,13 @@
     	                $document.unbind('mousemove.thumb');
     	                $document.unbind('mouseup.thumb');
     	            });
-    	        };   	    
+    	        };
+
+                var notifyOnChange = function(newValue) {
+                    if (typeof scope.onChange === 'function') {
+                        scope.onChange({value: newValue});
+                    }
+                };   	    
     	    }
     	};
     }
--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -1,5 +1,5 @@
  (function() {
-     function SongPlayer(Fixtures) {
+     function SongPlayer($rootScope, Fixtures) {
           
           var SongPlayer = {};
 
@@ -30,6 +30,12 @@
                   formats: ['mp3'],
                   preload: true
               });
+
+              currentBuzzObject.bind('timeupdate', function() {
+                  $rootScope.$apply(function() {
+                      SongPlayer.currentTime = currentBuzzObject.getTime();
+                  });
+              });
            
               SongPlayer.currentSong = song;
           };
@@ -43,6 +49,12 @@
           * @type {Object}
           */
           SongPlayer.currentSong = null;
+
+          /**
+          * @desc Current playback time (in seconds) of currently playing song
+          * @type {Number}
+          */
+          SongPlayer.currentTime = null;
 
 
           /**
@@ -129,8 +141,17 @@
                   setSong(song);
                   playSong(song);
               }
+          };
 
-
+          /**
+          * @function setCurrentTime
+          * @desc Set current time (in seconds) of currently playing song
+          * @param {Number} time
+          */
+          SongPlayer.setCurrentTime = function(time) {
+              if (currentBuzzObject) {
+                  currentBuzzObject.setTime(time);
+              }
           };
 
           return SongPlayer;
@@ -138,5 +159,5 @@
  
      angular
          .module('blocJams')
-         .factory('SongPlayer', SongPlayer);
+         .factory('SongPlayer', ['$rootScope', 'Fixtures', SongPlayer]);
  })();
